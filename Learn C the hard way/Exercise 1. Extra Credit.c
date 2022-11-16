b5f84d3c64eb63774eeab0e1cd23cbbae092085b
Fangli Kung <kungfangli@gmail.com>
2022-11-16 15:26:17 +0800

    add ex1

--- Learn C the hard way/Exercise 1. Extra Credit.c
+++ Learn C the hard way/Exercise 1. Extra Credit.c
@@ -5,5 +5,8 @@ int main(int atgc, char *argv[])
 	int distance = 100;
 	// this is also a comment
 	printf("You are %d miles away. \n",distance);
+	printf("You are 199 miles away. \n");
+	printf("You are 88 miles away. \n");
+	printf("You are 77 miles away. \n");
 	return 0;
 }
