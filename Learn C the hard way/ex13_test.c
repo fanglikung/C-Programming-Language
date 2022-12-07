709870803d8de0f9cdc0851bd1f70eedcc0729bb
fanglikung <fanglikung@gmail.com>
2022-12-08 04:21:11 +0800

    add ex13_test

--- Learn C the hard way/ex13_test.c
+++ Learn C the hard way/ex13_test.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
-	int main(int argc,char* argv[])
-	{	char str[] = "blachcqwe";
-		printf("sting = %s\n",str);
-		char st[] = {'b','l','\0'};
-		int i =0;
-		printf("size of char = %ld \n",sizeof(char));
-		
-		printf("size of st = %ld \n",sizeof(st));
-		for (i=0;st[i]!='\0';i++)
-			{
-				printf("s t r i n g = %c, %d \n",st[i],i);
-		}
-		return 0;
+int main(int argc,char* argv[])
+{	char str[] = "blachcqwe";
+	printf("sting = %s\n",str);
+	char st[] = {'b','l','\0'};
+	int i =0;
+	printf("size of char = %ld \n",sizeof(char));
+	
+	printf("size of st = %ld \n",sizeof(st));
+	for (i=0;st[i]!='\0';i++)
+		{
+			printf("s t r i n g = %c, %d \n",st[i],st[i]);
 	}
-
+	printf("b = %c \n",98);
+	return 0;
+}
 
 
 	
