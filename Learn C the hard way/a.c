bb53af650ac871c196b455ac4042b8bc5b6ad9ab
fanglikung <fanglikung@gmail.com>
2022-12-10 04:48:32 +0800

    update ex10

---
+++ Learn C the hard way/a.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+	switch('c'){
+		case 'a':
+		printf("a\n");
+		break;
+		case 'b':
+		printf("b\n");
+		break;
+		case 'c':
+		printf("c\n");
+		break;
+	}
+}
--- Learn C the hard way/ex10.c
+++ Learn C the hard way/ex10.c
@@ -1,26 +1,56 @@
 #include <stdio.h>
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-	int i = 0;
+    if (argc != 2) {
+        printf("ERROR: You need one argument.\n");
+        // this is how you abort a program
+        return 1;
+    }
 
-	// go through each string in argv
-	// why am I skipping argv[0]?
-	for (i = 1; i < argc; i++){
-		printf("arg %d: %s\n", i, argv[i]);
-	}
+    int i = 0;
+    for (i = 0; argv[1][i] != '\0'; i++) {
+        //char letter =tolower( argv[1][i]);
+        char letter =argv[1][i];
 
-	// let's make our own array of strings
-	char *states[] = {
-		"California","Oregon",
-		"Washington", "Texas"
-	};
-	
-	int num_states = 4;
+        switch (letter) {
+            case 'a':
+            case 'A':
+                printf("%d: 'A' and %d \n", i,letter);
+                break;
 
-	for (i = 0; i <num_states; i++){
-		printf("state %d: %s\n", i, states[i]);
-	}
-	
-	return 0;
+            case 'e':
+            case 'E':
+                printf("%d: 'E' and %d \n", i,letter);
+
+                break;
+
+            case 'i':
+            case 'I':
+                printf("%d: 'I'\n", i);
+                break;
+
+            case 'o':
+            case 'O':
+                printf("%d: 'O'\n", i);
+                break;
+
+            case 'u':
+            case 'U':
+                printf("%d: 'U'\n", i);
+                break;
+
+            case 'y':
+            case 'Y':
+                if (i > 2) {
+                    // it's only sometimes Y
+                    printf("%d: 'Y'\n", i);
+                }
+               // break;
+            default:
+                printf("%d: %c is not a vowel and letter of ASCII is %d \n", i, letter,letter);
+        }
+    }
+
+    return 0;
 }
